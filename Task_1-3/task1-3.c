e29d2553504694c7f3e1fca26df0e80844a8aee0
RomanKostin <romankostin22019@gmail.com>
2023-11-13 11:08:08 +0300

    Задание 1.3

---
+++ Task_1-3/task1-3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <float.h>
+/**
+*@brief function for user input
+*@param *input user's input
+*/
+void input(double *input);
+/**
+*@brief function checks if inputs are right
+*@param Length - length of the wire
+*@param Cross_sectional_area - cross sectional area of the wire
+*@param Resistivity - resistivity of the metal which is used in the wire
+*/
+void check_input(double Length, double Cross_sectional_area, double Resistivity);
+/**
+*@brief function for finding resistivity of the wire (length * resistivity of the metal) divided by cross sectional area, which is turned in meters
+*@param Length - length of the wire
+*@param Cross_sectional_area - cross sectional area of the wire
+*@param Resistivity - resistivity of the metal which is used in the wire
+*@return resistivity of the wire
+*/
+double find_resistance(double Length, double Cross_sectional_area, double Resistivity);
+int main()
+{
+	double Length, Cross_sectional_area, Resistivity;
+	printf_s("input length (meters), cross sectional area (milimeters^2) and resistivity of any metal (aluminium is 0.028) ");
+	input(&Length);
+	input(&Cross_sectional_area);
+	input(&Resistivity);
+	check_input(Length, Cross_sectional_area, Resistivity);
+	printf_s("Resistance of the wire %lf ohms",find_resistance(Length,Cross_sectional_area,Resistivity));
+	return 0;
+}
+void input(double *input)
+{
+	scanf_s("%lf", input);
+}
+void check_input(double Length, double Cross_sectional_area, double Resistivity)
+{
+	if (Length <= DBL_EPSILON || Cross_sectional_area <= DBL_EPSILON || Resistivity <= DBL_EPSILON)
+	{
+		printf_s("Wrong inputs!");
+		abort();
+	}
+}
+double find_resistance(double Length, double Cross_sectional_area, double Resistivity)
+{
+	return (Length * Resistivity) / (Cross_sectional_area*0.000001);
+}
