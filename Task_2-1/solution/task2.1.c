39a57fc60b3daf3fa11805491a9ab8e1caf76f54
RomanKostin <romankostin22019@gmail.com>
2023-12-05 20:49:59 +0300

    исправление 2.1(новое)

---
+++ Task_2-1/solution/task2.1.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+/**
+* @brief enum type used to choose formula
+*/
+enum formula { N = 1, DBL_N, TPL_N };
+
+/**
+* @brief function used to get value from user
+* @return temp - result of user's input
+*/
+double input();
+
+/*
+* @brief entry point into the programm
+* @return 0 if programm works correctly
+*/
+int main()
+{
+	double a, n, choice;
+	printf_s("%s", "choose formula 1) N; 2) 2N; 3) 3N ");
+	choice = input();
+	printf_s("input A = ");
+	a = input();
+	printf_s("input N = ");
+	n = input();
+	enum formula equation = (enum formula)(choice);
+	switch (equation)
+	{
+	case N:
+	{
+		printf_s("the answer is %lf", a * n);
+		return 0;
+	}
+	case DBL_N:
+	{
+		printf_s("the answer is %lf", a * n * 2);
+		return 0;
+	}
+	case TPL_N:
+	{
+		printf_s("the answer is %lf", a * n * 3);
+		return 0;
+	}
+	default:
+	{
+		errno = EIO;
+		perror("Input error!");
+		return 0;
+	}
+	}
+}
+
+double input()
+{
+	double temp;
+	int res = scanf_s("%lf", &temp);
+	if (res != 1)
+	{
+		errno = EIO;
+		perror("wrong input");
+		abort();
+	}
+	return temp;
+}
