9bf1a72d551f856868b096ffa12573da40c075ef
RomanKostin <147978622+RomanKostin@users.noreply.github.com>
2023-12-24 14:59:57 +0300

    Задание 1.2

--- Task_1-2/task1.2.c
+++ Task_1-2/task1.2.c
@@ -15,9 +15,8 @@ double input();
 * @brief function checks if triangle exist
 * @param side_a length of the first side
 * @param side_b length of the second side
-* @param angle_ab angle between two sides of a triangle
 */
-void check_for_existance(double side_a, double side_b, double angle_ab);
+void check_for_existance(double side_a, double side_b);
 
 /**
 * @brief formula for finding the third side of a triangle
@@ -64,7 +63,7 @@ int main()
 	side_b = input();
 	angle_ab = input();
 	check_angle(angle_ab);
-	check_for_existance(side_a, side_b, angle_ab);
+	check_for_existance(side_a, side_b);
 	printf_s("%lf\n%lf\n%lf\n", find_side(side_a, side_b, angle_ab), find_area(side_a, side_b, angle_ab), find_radius(side_a, side_b, angle_ab));
 	return 0;
 }
@@ -100,9 +99,8 @@ double input()
 	return temp;
 }
 
-void check_for_existance(double side_a, double side_b, double angle_ab)
+void check_for_existance(double side_a, double side_b)
 {
-	double const math_alg = M_PI / 180;
 	if (side_a <= DBL_EPSILON || side_b <= DBL_EPSILON)
 	{
 		printf_s("Triangle doesn't exist");
