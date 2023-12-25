d802e1d71894cbc44c2e4c519a5ad98fe289ac83
RomanKostin <147978622+RomanKostin@users.noreply.github.com>
2023-12-25 22:33:56 +0300

    Задание 4.1

--- Task_4-1/solution/task4-1.c
+++ Task_4-1/solution/task4-1.c
@@ -6,6 +6,45 @@
 #include <assert.h>
 
 /**
+*@brief function find first min index of the array
+*@param array - pointer to the array
+*@param size - size of the array
+*@return j - min index
+*/
+size_t findMinIndex(int* const array, size_t const size);
+
+/**
+*@brief function find first max index of the array
+*@param array - pointer to the array
+*@param size - size of the array
+*@return j - max index
+*/
+size_t findMaxIndex(int* const array, size_t const size);
+
+/**
+*@brief funtion checks if left border is less than right(if it false --> abort();)
+*@param left - left border of domain
+*@param right - right border of domain
+*/
+void checkDomain(int const left, int const right);
+
+/**
+*@brief funtion checks if some value belongs to the domain that user chose.
+*@param value - some value
+*@param left - left border of domain
+*@param right - right border of domain
+*/
+void checkBelongingDomain(int value, int const left, int const right);
+
+/**
+*@brief function for random array fill
+*@param array - pointer to array
+*@param size - size of array
+*@return arrayCopy - copy of an original array
+*/
+int* copy(int* const array, size_t const size);
+
+/**
 *@brief function for int value
 *@return entered_number - value of entered number
 */
@@ -22,21 +61,25 @@ size_t getSize_t();
 *@param size - size of array
 *@return array - pointer to an empty array
 */
-int* getMemory(size_t size);
+int* getMemory(size_t const size);
 
 /**
 *@brief function for filling array with user's input
 *@param array - pointer to array
 *@param size - size of array
+*@param left - left border of domain
+*@param right - right border of domain
 */
-void keyboadArray(int* const array, const size_t size);
+void keyboadArray(int* const array, const size_t size, int const left, int const right);
 
 /**
 *@brief function for random array fill
 *@param array - pointer to array
 *@param size - size of array
+*@param left - left border of domain
+*@param right - right border of domain
 */
-void randomArray(int* const array, const size_t size);
+void randomArray(int* const array, const size_t size, int const left, int const right);
 
 /**
 *@brief prints values of array
@@ -67,25 +110,13 @@ int firstTask(int* const array, size_t const size);
 void secondTask(int* const array, size_t const size);
 
 /**
-*@brief function that swaps values
-*@param a - first value
-*@param b - second value
-*/
-void swap(int* const a, int* const b);
-
-/**
-*@brief function sorts values of array from smallest number to biggest
-*@param array - pointer to array
-*@param size - size of array
-*/
-void arraySort(int* const array, size_t const size);
-
-/**
 *@brief function swaps all values of array to 0 between smallest and biggest numbers
 *@param array - pointer to array
 *@param size - size of array
+*@param minIndex - index of the first min value of an array
+*@param maxIndex - index of the first max value of an array
 */
-void thirdTask(int* const array, size_t const size);
+void thirdTask(int* const array, size_t const size, size_t minIndex, size_t maxIndex);
 
 /**
 *@brief used for choice of a way to fill an array
@@ -106,32 +137,68 @@ int main()
 {
     printf_s("Input size of array: ");
     size_t size = getSize_t();
+    printf_s("input your domain\n");
+    int const left = getInt(),
+        right = getInt();
+    checkDomain(left, right);
     printf_s("1 - keyboard fill; 2 - random fill ");
     int choice = getInt();
     int* array = getMemory(size);
     switch ((enum Choices)choice)
     {
     case RANDOM:
-        randomArray(array, size);
+        randomArray(array, size, left, right);
         break;
     case KEYBOARD:
         printf_s("Enter array elements:\n");
-        keyboadArray(array, size);
+        keyboadArray(array, size, left, right);
         break;
     default:
         printf("Make a valid choice!\n");
         return 1;
     }
-    arraySort(array, size);
     output(array, size);
-    printf_s("\nnegative sum of even numbers: %d\n", firstTask(array, size));
-    printf_s("indexes, that can be devided by 6: \n");
+    int* arrayCopy = copy(array,size);
+    // Task 1
+    printf_s("\n\nnegative sum of even numbers: %d\n", firstTask(array, size));
+    // Task 2
+    printf_s("\nindexes, that can be devided by 6: \n");
     secondTask(array, size);
-    printf_s("\n new array: \n");
-    thirdTask(array, size);
-    output(array, size);
+    // Task 3
+    printf_s("\n\nmin index %zu; max index %zu \n", findMinIndex(array,size), findMaxIndex(array,size));
+    thirdTask(arrayCopy,size,findMinIndex(array,size),findMaxIndex(array,size));
+    output(arrayCopy,size);
+    freeArray(arrayCopy);
     freeArray(array);
     return 0;
+}
+
+void checkDomain(int const left, int const right)
+{
+    if (left > right)
+    {
+        printf_s("wrong domain!!!!");
+        abort();
+    }
+}
+
+void checkBelongingDomain(int value, int const left, int const right)
+{
+    if (value<left || value>right)
+    {
+        printf_s("value doesn't belong to domain!");
+        abort();
+    }
+}
+
+int* copy(int* const array, size_t size)
+{
+    int* arrayCopy = (int*)malloc(size * sizeof(int));
+    for (size_t i = 0; i < size; i++)
+    {
+        arrayCopy[i] = array[i];
+    }
+    return arrayCopy;
 }
 
 int getInt()
@@ -158,7 +225,7 @@ size_t getSize_t()
     return (size_t)entered_number;
 }
 
-int* getMemory(size_t size)
+int* getMemory(size_t const size)
 {
     int* array = (int*)malloc(size * sizeof(int));
     if (NULL == array)
@@ -168,20 +235,21 @@ int* getMemory(size_t size)
     return array;
 }
 
-void keyboadArray(int* const array, const size_t size)
+void keyboadArray(int* const array, const size_t size, int const left, int const right)
 {
     for (size_t i = 0; i < size; i++)
     {
         array[i] = getInt();
+        checkBelongingDomain(array[i], left, right);
     }
 }
 
-void randomArray(int* const array, const size_t size)
+void randomArray(int* const array, const size_t size, int const left, int const right)
 {
     srand(time(NULL));
     for (size_t i = 0; i < size; i++)
     {
-        array[i] = rand() % 2001 - 1000;
+        array[i] = rand() % (right - left + 1) + left;
     }
 }
 
@@ -226,31 +294,46 @@ void secondTask(int* const array, size_t size)
     }
 }
 
-void swap(int* const a, int* const b)
+void thirdTask(int* const array, size_t const size, size_t minIndex, size_t maxIndex)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void arraySort(int* const array, size_t const size)
-{
-    for (size_t i = 0; i < size - 1; i++)
+    if (minIndex > maxIndex)
     {
-        for (size_t j = 0; j < size - i - 1; j++)
+        for (maxIndex; maxIndex < minIndex-1; maxIndex++)
         {
-            if (array[j] > array[j + 1])
-            {
-                swap(&array[j], &array[j + 1]);
-            }
+            array[maxIndex+1] = 0;
+        }
+    }
+    else
+    {
+        for (minIndex; minIndex < maxIndex-1; minIndex++)
+        {
+            array[minIndex+1] = 0;
         }
     }
 }
 
-void thirdTask(int* const array, size_t const size)
+size_t findMinIndex(int* const array, size_t const size)
 {
-    for (size_t i = 1; i < size - 1; i++)
+    size_t j = 0;
+    for (size_t i = 1; i < size; i++)
     {
-        array[i] = 0;
+        if (array[j] > array[i])
+        {
+            j = i;
+        }
     }
+    return j;
+}
+
+size_t findMaxIndex(int* const array, size_t const size)
+{
+    size_t j = 0;
+    for (size_t i = 1; i < size; i++)
+    {
+        if (array[j] < array[i])
+        {
+            j = i;
+        }
+    }
+    return j;
 }
